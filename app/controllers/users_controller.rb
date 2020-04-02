7bcfc59edb0cc058f475ac8438a5467883b28c24
yuki takagi <yuki99takagi@gmail.com>
2020-04-02 13:19:16 +0900

    ユーザー編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,34 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(user_params)
-    if @user.save
+    @user = User.new(user_params) #user_paramsでcPOSTデータをチェック
+    if @user.save #保存できていたら
+      flash[:success] = '新しいユーザーを登録しました。'
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
+      flash.now[:danger] = 'ユーザーの登録に失敗しました。'
       render :new
     end
   end
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました！'
+        redirect_to user_path(@user.id)
+      else
+        flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end
+  end
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! } #バリデーション判定前にemail属性を強制的に小文字に変換
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
   has_many :posts
   has_many :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
